6c4937dfd9ad5c011d28d52a255d1766d9b5e58b
David Hall <david.hall@tv4.se>
2011-05-05 14:17:25 +0200

    Know your basic regex rules. :(

--- lib/options.rb
+++ lib/options.rb
@@ -19,10 +19,8 @@ class Options
   end
 
   def check_parameters
-    puts 'Hello'
-    check_param('resize',/^[0-9]{1,5}(x[0-9]{1,5})+$/)
-    print 'Bo '
-    check_param('thumbnail',/^[0-9]{1,5}(x[0-9]{1,5})+$/)
+    check_param('resize',/^[0-9]{1,5}(x[0-9]{1,5})?$/)
+    check_param('thumbnail',/^[0-9]{1,5}(x[0-9]{1,5})?$/)
     check_param('rotate',/^(-)?[0-9]{1,3}(\.[0-9]+)?$/)
     check_param('format',/^[0-9a-zA-Z]{2,6}$/)
     check_param('progressive',/^true|false$/)
