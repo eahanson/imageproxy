d1fb139fc29aa1affcdc2f361fa5ccb7157139af
Andy Blyler <andy@blyler.cc>
2011-05-05 21:16:30 -0400

    Change signatures to be URL safe Base64

--- lib/signature.rb
+++ lib/signature.rb
@@ -3,7 +3,7 @@ require 'openssl'
 
 class Signature
   def self.create(path, secret)
-    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), secret, remove_signature_from(path))).strip
+    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), secret, remove_signature_from(path))).strip.tr('+/', '-_')
   end
 
   def self.remove_signature_from(path)
